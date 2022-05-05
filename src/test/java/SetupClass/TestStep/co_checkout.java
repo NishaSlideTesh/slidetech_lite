70a6cc55ca270628d6a76b7ea65fa8d9454e3c08
Nisha Dhiman <91591950+nishaslidetech@users.noreply.github.com>
2022-05-05 22:06:38 +0530

    Update co_checkout.java

--- src/test/java/SetupClass/TestStep/co_checkout.java
+++ src/test/java/SetupClass/TestStep/co_checkout.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import org.junit.Assert;
 import SetupClass.Set;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -216,6 +216,12 @@ public class co_checkout extends Set{
 		js.executeScript("arguments[0].scrollIntoView();", continue_delete);
 		continue_delete.click();
 		Thread.sleep(2000);
+		
+		String verifyDeleteAccount = wait
+				.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@data-bind='html: $parent.prepareMessageForHtml(message.text)']"))).getText();
+		 Thread.sleep(3000);
+	         Assert.assertTrue("Account is not deleted", verifyDeleteAccount.contains("Your account has been deleted successfully."));
+	         System.out.println("your account delete successfully");
 	}
 
 
