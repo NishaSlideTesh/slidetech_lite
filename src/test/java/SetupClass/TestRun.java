e6255ef2293b0f83b9a836c39247b9f668d8852d
nishaslidetech <nisha.dhiman@slidetech.in>
2023-01-25 11:45:44 +0530

    update

--- src/test/java/SetupClass/TestRun.java
+++ src/test/java/SetupClass/TestRun.java
@@ -8,9 +8,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "." }, tags = ("@Sign_Up_Correct_Data"), plugin = { "pretty",
-		"html:target/site/cucumber-pretty", "json:target/cucumber/cucumber.json", "usage:target/usage.jsonx",
-		"junit:target/cucumber.xml" })
+@CucumberOptions(features = { "." }, // tags = ("@Sign_Up_Correct_Data"),
+		plugin = { "pretty", "html:target/site/cucumber-pretty", "json:target/cucumber/cucumber.json",
+				"usage:target/usage.jsonx", "junit:target/cucumber.xml" })
 
 public class TestRun {
 
